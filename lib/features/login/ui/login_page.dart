0aff386d8e1c4873a8efe40684b9fae1a7da5922
sofiyakamalova <sofiyakamalova1805@gmail.com>
2024-01-29 16:11:31 +0600

    fixed bags

--- lib/features/login/ui/login_page.dart
+++ lib/features/login/ui/login_page.dart
@@ -5,7 +5,7 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
         Text('Login Page'),
         Text('Sign In'),
